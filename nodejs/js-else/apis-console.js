8e93e02e26afd30009ca3badeb4f8eda5ec3365e
zbs <90139766+zbs-test@users.noreply.github.com>
2022-12-26 22:53:46 +0800

    Create apis-console.js

---
+++ nodejs/js-else/apis-console.js
@@ -0,0 +1,12 @@
+console.log("some ouputs");
+console.error("some errors outputs");
+console.info('ctrl + click to see more info about interface Console');
+console.assert(true);   //if true, no expection
+console.dir('item: this can be optional', 'option: this para is optiomal');
+console.warn('warn')
+console.debug('debug 优于 warn 输出')
+console.info("info 优于 warn, 低于 debug");
+console.clear();
+console.count('label:标记为1');
+console.count('label:second label');
+console.groupEnd()
