791932629d6007a7a61e7ab51f0483db5500ea06
zbs <90139766+zbs-test@users.noreply.github.com>
2022-12-26 23:12:21 +0800

    Create events.js

---
+++ nodejs/js-else/events.js
@@ -0,0 +1,25 @@
+# DOM 理解为html里的一切数据，含标签属性(含事件属性)...
+# some important events 
+
+事件	       描述
+onchange	  HTML 元素改变
+onclick	    用户点击 HTML 元素
+onmouseover	鼠标指针移动到指定的元素上时发生
+onmouseout	用户从一个 HTML 元素上移开鼠标时发生
+onkeydown	  用户按下键盘按键
+onload      浏览器已完成页面的加载
+事件可以用于处理表单验证，用户输入，用户行为及浏览器动作:
+
+页面加载时触发事件
+页面关闭时触发事件
+用户点击按钮执行动作
+验证用户输入内容的合法性
+...
+可以使用多种方法来执行 JavaScript 事件代码：
+
+HTML 事件属性可以直接执行 JavaScript 代码
+HTML 事件属性可以调用 JavaScript 函数
+你可以为 HTML 元素指定自己的事件处理程序
+你可以阻止事件的发生。
+...
+//done
