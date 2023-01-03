79aec13fb1c72fd33153394f618311bcf8bbe015
zbs <90139766+zbs-test@users.noreply.github.com>
2023-01-03 14:15:35 +0800

    Create test.fsx

---
+++ F#/test.fsx
@@ -0,0 +1,12 @@
+// this is comment 
+(*still comment*)
+
+// let's define a immutable variable and give it a type as `int`
+let a:int = 1 
+// now let's print a and using format and endline which shows 'n'
+printfn "%d" a 
+// check above function and let's define a new function 
+let addTwoNum (a:int) (b:int) :int = a + b 
+// use this function, check it if result is 3
+let result = addTwoNum 1 2 
+printfn "%d " result // if can print 
